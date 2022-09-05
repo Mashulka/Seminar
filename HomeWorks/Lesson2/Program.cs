fc83c74542834cef7da558637f91079313334eee
Mariya Titova <mariya-titova@list.ru>
2022-09-05 21:08:02 +0300

    да уж

--- HomeWorks/Lesson2/Program.cs
+++ HomeWorks/Lesson2/Program.cs
@@ -101,3 +101,25 @@ Console.Write("Введите любое целое число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 cube(number);
 */
+
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("Возведи число A в натуральную степень B");
+
+int Exponentiation(int numberA, int numberB)
+{
+    int result = 1;
+    for(int i=1; i <= numberB; i++)
+    {
+        result = result * numberA;
+    }
+ return result;
+}
+
+ Console.Write("Введите число A: ");
+ int numberA = Convert.ToInt32(Console.ReadLine());
+ Console.Write("Введите число B: ");
+ int numberB = Convert.ToInt32(Console.ReadLine());
+
+int exponentiation = Exponentiation(numberA, numberB);
+Console.WriteLine("Ответ: " + exponentiation); 
