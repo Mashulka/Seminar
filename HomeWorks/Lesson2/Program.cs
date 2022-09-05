e5b992f84edaa8b6fc76f5d9cd9473cf7a7fe870
Mariya Titova <mariya-titova@list.ru>
2022-09-05 22:21:44 +0300

    запиши

--- HomeWorks/Lesson2/Program.cs
+++ HomeWorks/Lesson2/Program.cs
@@ -101,7 +101,7 @@ Console.Write("Введите любое целое число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 cube(number);
 */
-
+/*
 // Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
 Console.WriteLine("введите число A");
@@ -116,3 +116,19 @@ for (int i = 1; i < b; i++)
 }
 
 Console.WriteLine("A в степени B равно: " + step);
+*/
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число: ");
+int i = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (i > 0)
+{
+    int num = i % 10;
+    i = i / 10;
+    sum = sum + num;
+}
+
+Console.WriteLine("Сумма цифр в числе равна: " + sum);
